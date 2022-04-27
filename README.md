d2659b5c70077d0980439a43e192ef3a77ded8c0
Keith <wygulmage@users.noreply.github.com>
2022-04-27 12:25:06 -0400

    removed `IO` instance, updated laws
    For IO's `<|>` and `mplus` to be valid, they should have special handling for
    `mzero` to prefer other errors. Until that's the case, `IO` can't be
    `empty` (and its `MonadPlus` and `Alternative` instances are illegal).

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # emptiable
+
+This project provides the class `Emptiable` in the module Data.Emptiable for containers that can be empty.
+
+Because of its extreme simplicity, it's offered under a public domain equivalent license.
--- src/Data/Emptiable.hs
+++ src/Data/Emptiable.hs
@@ -21,17 +21,17 @@ Laws:
 > empty '<*>' mx  =  empty '<*' mx
 > mf '<*>' empty  =  mf '*>' empty
 > empty '<*>' mx '<*>' empty  =  empty
-The last one tells you that 'empty' must always be an anihilative zero for 'Applicative' multiplication rather than a unit, but acknowledges that effects performed before reaching 'empty' may be impossible to undo. So, for example, we'd need a (hypothetical) @class (Semigroup a)=> Anihilative a where nihil :: a@ to have @empty :: (Anihilative c)=> Const c a@.
+'empty' must always be an anihilative zero for 'Applicative' multiplication rather than a unit, but effects performed before reaching 'empty' may be impossible to undo.
 
 > Monad m => empty '>>=' f = empty -- "free"
 
 > Foldable m => 'null' empty == True -- "free"
 
-> Alternative m => empty '<|>' mx = mx
-This can't require @mx <|> empty = mx@, because, e.g., @'throwIO' "Whoops!" <|> empty@ is @empty@ rather than @'throwIO' "Whoops!"@.
+> Alternative m => empty '<|>' mx  =  mx  =  mx '<|>' empty
+The Alternative requirement implies that 'empty' = 'Control.Applicative.empty'.
 
-> MonadPlus m => mplus empty mx = mx
-This can't require @mplus mx empty = mx@, because, e.g., @mplus ('throwIO' "Whoops!") empty@ is @empty@ rather than @'throwIO' "Whoops!"@.
+> MonadPlus m => 'mplus' empty mx = mx = 'mplus' mx empty
+The MonadPlus requirement implies that 'empty' = 'Control.Monad.mzero'.
 -}
    empty :: m a
 
@@ -40,7 +40,7 @@ instance Emptiable Proxy where empty = Proxy
 instance Emptiable Maybe where empty = Nothing
 instance Emptiable [] where empty = []
 instance Emptiable ZipList where empty = ZipList empty
-instance Emptiable IO where empty = Alternative.empty
+-- instance Emptiable IO where empty = Alternative.empty -- This instance will be invalid until (<|>) and mplus have special handling for mzero.
 instance Emptiable ReadP.ReadP where empty = ReadP.pfail
 instance Emptiable ReadPrec.ReadPrec where empty = ReadPrec.pfail
 instance (Emptiable m)=> Emptiable (Functor.Compose m n) where empty = Functor.Compose empty
