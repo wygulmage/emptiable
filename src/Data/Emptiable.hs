ec3d0f6302273b5dbbd411188a6d985143b1731e
wygulmage <wygulmage@users.noreply.github.com>
2022-05-04 13:22:31 -0400

    added Containers instances; deleted cabal.project
    Empty cabal.project was ignored by stack but causing erronious errors in cabal.

--- src/Data/Emptiable.hs
+++ src/Data/Emptiable.hs
@@ -3,18 +3,32 @@ module Data.Emptiable (Emptiable (..)) where
 
 import Control.Applicative hiding (Alternative (..))
 import qualified Control.Applicative as Alternative
+import Data.Monoid
 import qualified Data.Functor.Compose as Functor
 import qualified Data.Functor.Product as Functor
 import Data.Proxy
 import Text.ParserCombinators.ReadP as ReadP
 import Text.ParserCombinators.ReadPrec as ReadPrec
+-- GHC
 import GHC.Generics
+-- Containers
+import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
 
 
 class Emptiable m where
 {-^ @empty :: m a@ does not contain any @a@s.
 All of the laws should be obvious. Most of the laws are "free" if @m@ isn't a GADT.
 Laws:
+
+prop> 'seq' empty () = ()
+@empty@ can't be bottom in weak head normal form. It can evaluate to bottom (nonterminating or error) in a specific context. (For example, 'throwIO' could be used to create a valid @empty@ for @IO@; 'error' could not.)
+
+> Foldable m
+prop> 'null' empty == True -- "free"
+
 @'Functor' m@
 prop> \ f -> fmap f empty = empty -- "free"
 
@@ -27,16 +41,12 @@ prop> \ mx -> empty '<*>' mx '<*>' empty  =  empty
 > Monad m
 prop> \ f -> empty '>>=' f = empty -- "free"
 
-> Foldable m
-prop> 'null' empty == True -- "free"
-
 > Alternative m
 prop> \ mx -> empty '<|>' mx  =  mx  =  mx '<|>' empty
-The Alternative requirement implies that 'empty' = 'Control.Applicative.empty'.
-
+The 'Alternative' requirement implies that 'empty' = 'Control.Applicative.empty'.
 > MonadPlus m
 prop> \ mx -> 'mplus' empty mx = mx = 'mplus' mx empty
-The MonadPlus requirement implies that 'empty' = 'Control.Monad.mzero'.
+The 'MonadPlus' requirement implies that 'empty' = 'Control.Monad.mzero'.
 -}
    empty :: m a
 
@@ -48,6 +58,8 @@ instance Emptiable ZipList where empty = ZipList empty
 -- instance Emptiable IO where empty = Alternative.empty -- This instance will be invalid until (<|>) and mplus have special handling for mzero.
 instance Emptiable ReadP.ReadP where empty = ReadP.pfail
 instance Emptiable ReadPrec.ReadPrec where empty = ReadPrec.pfail
+instance (Emptiable m)=> Emptiable (Alt m) where empty = Alt empty
+instance (Emptiable m)=> Emptiable (Ap m) where empty = Ap empty
 instance (Emptiable m)=> Emptiable (Functor.Compose m n) where empty = Functor.Compose empty
 instance (Emptiable m, Emptiable n)=> Emptiable (Functor.Product m n) where empty = Functor.Pair empty empty
 
@@ -58,3 +70,9 @@ instance (Emptiable m)=> Emptiable (M1 i c m) where empty = M1 empty
 -- no instance for (:+:) because we don't know which empty to use.
 instance (Emptiable m, Emptiable n)=> Emptiable ((:*:) m n) where empty = empty :*: empty
 instance (Emptiable m)=> Emptiable ((:.:) m n) where empty = Comp1 empty
+
+--- Containers Instances ---
+instance Emptiable IntMap.IntMap where empty = IntMap.empty
+instance Emptiable (Map.Map i) where empty = Map.empty
+instance Emptiable Seq.Seq where empty = Seq.empty
+instance Emptiable Set.Set where empty = Set.empty
